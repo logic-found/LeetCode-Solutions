0c217a3654fcce6865915e7dc208e60391ad9b53
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-13 22:29:52 +0530

    Time: 146 ms (18.81%), Space: 39.4 MB (49.49%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -4,9 +4,10 @@ public:
         int n=values.size();
         int prev=values[0],ans=0;
         
+        // prev=nums[i]+i   curr=nums[j]-j
         for(int j=1;j<n;++j){
-            int x=values[j]-j;
-            ans=max(ans,prev+x);
+            int curr=values[j]-j;
+            ans=max(ans,prev+curr);
             if(values[j]+j>prev) prev=values[j]+j;
         }
         return ans;
