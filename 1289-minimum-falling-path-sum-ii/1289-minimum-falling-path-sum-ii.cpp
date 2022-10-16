131cb9dd583efa0a43a3e53f5bfe15c3a6205395
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-16 10:52:19 +0530

    Time: 1130 ms (35.14%), Space: 15.9 MB (36.56%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int recur(int i,int prev,int& m,int& n,vector<vector<int>>& mat,vector<vector<int>>& dp){
+        if(i>=(m-1)) return dp[i+1][prev+1]=0;
+        
+        if(dp[i+1][prev+1]!=-1) return dp[i+1][prev+1];
+        int temp=INT_MAX;
+        
+        for(int j=0;j<n;++j){
+            if(j==prev) continue;
+            temp=min(temp,mat[i+1][j]+recur(i+1,j,m,n,mat,dp));
+        }
+        return dp[i+1][prev+1]=temp; 
+    }
+    int minFallingPathSum(vector<vector<int>>& grid) {
+       int m=grid.size(),n=grid[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        int ans= recur(-1,-1,m,n,grid,dp);
+        
+        return ans;
+    }
+};
