ad2d02a1ea763b1bd4a3205b1ca4dc27891d71c7
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-16 09:54:13 +0530

    Time: 42 ms (13.68%), Space: 10.1 MB (68.84%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+    // space optimized
     int minFallingPathSum(vector<vector<int>>& mat) {
         int m=mat.size(),n=mat[0].size(),ans=INT_MAX;
-        vector<vector<int>> dp(m,vector<int>(n));
+        vector<int> prev(n);
         for(int j=0;j<n;++j){
-            dp[m-1][j]=mat[m-1][j];    //initialization
+            prev[j]=mat[m-1][j];    //initialization
         }
         
         for(int i=m-2;i>=0;--i){
+            vector<int> temp(n);
             for(int j=0;j<n;++j){
-                int x=(j-1>=0)? dp[i+1][j-1]:100000;
-                int y=(j+1<n)? dp[i+1][j+1]:100000;
-                dp[i][j]=mat[i][j]+min(dp[i+1][j],min(x,y));
-            }  
+                int x=(j-1>=0)? prev[j-1]:100000;
+                int y=(j+1<n)? prev[j+1]:100000;
+                temp[j]=mat[i][j]+min(prev[j],min(x,y));
+            } 
+            prev=temp;
         }
         for(int j=0;j<n;++j){
-            ans=min(ans,dp[0][j]);
+            ans=min(ans,prev[j]);
         }
         
         return ans;
