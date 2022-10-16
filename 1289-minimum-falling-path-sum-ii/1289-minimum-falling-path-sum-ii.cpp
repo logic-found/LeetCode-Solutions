a864d8f98d12eea77e978f5749c98a3cdf0a4320
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-16 12:06:02 +0530

    Time: 932 ms (45.26%), Space: 15.7 MB (54.57%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    //space optimised
     int minFallingPathSum(vector<vector<int>>& mat) {
         int m=mat.size(),n=m;
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
-        
-        for(int j=0;j<=n;++j) dp[m][j]=0;   //initialization
+        vector<int> dp(n+1,0);
         
         for(int i=m-1;i>=0;--i){
+            vector<int> auxi(n+1);
             for(int prev=0;prev<=n;++prev){  
                 int temp=INT_MAX;
                 
                 for(int j=0;j<n;++j){
                     if(prev==j) continue;
-                    temp=min(temp,mat[i][j]+dp[i+1][j]);
+                    temp=min(temp,mat[i][j]+dp[j]);
                 }
-                dp[i][prev]=temp;
+                auxi[prev]=temp;
             }
+            dp=auxi;
         }
-        return dp[0][n];    // storing th min of each row at nth column
+        return dp[n];    // storing th min of each row at nth column
     }
 };
