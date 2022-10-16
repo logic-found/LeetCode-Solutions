a1b4591988cbfd08358e5e5250bb4486de8eb313
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-16 09:51:52 +0530

    Time: 38 ms (23.66%), Space: 10.3 MB (55.04%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int recur(int i,int j,int& m,int& n,vector<vector<int>>& mat,vector<vector<int>>& dp){
-        if(i<0 || j<0 || i>=m || j>=n) return 100000;
-        if(i==m-1) return dp[i][j]=mat[i][j];
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        return dp[i][j]=mat[i][j]+min(recur(i+1,j,m,n,mat,dp),
-                            min(recur(i+1,j-1,m,n,mat,dp),recur(i+1,j+1,m,n,mat,dp)));
-    }
     int minFallingPathSum(vector<vector<int>>& mat) {
         int m=mat.size(),n=mat[0].size(),ans=INT_MAX;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n));
+        for(int j=0;j<n;++j){
+            dp[m-1][j]=mat[m-1][j];    //initialization
+        }
         
-        for(int i=0;i<n;++i){
-            ans=min(ans,recur(0,i,m,n,mat,dp));
+        for(int i=m-2;i>=0;--i){
+            for(int j=0;j<n;++j){
+                int x=(j-1>=0)? dp[i+1][j-1]:100000;
+                int y=(j+1<n)? dp[i+1][j+1]:100000;
+                dp[i][j]=mat[i][j]+min(dp[i+1][j],min(x,y));
+            }  
+        }
+        for(int j=0;j<n;++j){
+            ans=min(ans,dp[0][j]);
         }
         
         return ans;
