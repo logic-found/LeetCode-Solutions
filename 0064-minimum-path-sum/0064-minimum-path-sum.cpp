d762c4dbb875fd970c2c900380363c0d5bbb8569
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-20 21:28:04 +0530

    Time: 24 ms (27.55%), Space: 10.2 MB (31.40%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int recur(int i,int j,int& m,int& n,vector<vector<int>>& grid,vector<vector<int>>& dp){
-        if(i>=m || j>=n) return 300;
-        if(i==m-1 && j==n-1) return dp[i][j]=grid[i][j];
+    int recur(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(i<0 || j<0) return 300;
+        if(i==0 && j==0) return dp[i][j]=grid[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
         
-        int temp=grid[i][j]+min(recur(i+1,j,m,n,grid,dp),recur(i,j+1,m,n,grid,dp));
+        int temp=grid[i][j]+min(recur(i-1,j,grid,dp),recur(i,j-1,grid,dp));
         return dp[i][j]=temp;
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return recur(0,0,m,n,grid,dp);
+        return recur(m-1,n-1,grid,dp);
     }
 };
