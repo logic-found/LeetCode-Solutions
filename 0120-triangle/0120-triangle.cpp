0de955f3d927b73de6878673015b18c676fb1ea2
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-20 19:20:49 +0530

    Time: 15 ms (34.97%), Space: 9 MB (31.72%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int recur(int i,int j,int& m,vector<vector<int>>& tri,vector<vector<int>>& dp){
-        if(i>=m) return 100000;
-        if(i==m-1) return dp[i][j]=tri[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int temp=tri[i][j]+min(recur(i+1,j,m,tri,dp),recur(i+1,j+1,m,tri,dp));
-        return dp[i][j]=temp;
-        
-    }
     int minimumTotal(vector<vector<int>>& tri) {
       int m=tri.size();  
-      vector<vector<int>> dp(m,vector<int>(m,-1));
-      return recur(0,0,m,tri,dp);
+      vector<vector<int>> dp(m+1,vector<int>(m+1,0));
+        
+      for(int i=m-1;i>=0;--i){
+          for(int j=0;j<=i;++j){
+            dp[i][j]=tri[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+          }
+      }
+        return dp[0][0]; 
     }
 };
