259f0389bc981699c9628332874a46c235a4fff1
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-22 23:09:09 +0530

    Time: 32 ms (40.10%), Space: 9 MB (60.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int recur(int i,int j,string& s1,string& s2,vector<vector<int>>& dp){
-        if(j<0 && i>=0) return i+1;    // delete all left over
-        if(i<0 && j>=0) return j+1;    // insert all left over
-        if(j<0 || i<0) return 0;
+        if(j<0) return i+1;    // delete all left over
+        if(i<0) return j+1;    // insert all left over
         if(dp[i][j]!=-1) return dp[i][j];
         
         if(s1[i]==s2[j]){
@@ -20,8 +19,6 @@ public:
     }
     int minDistance(string s1, string s2) {
         int n1=s1.size(),n2=s2.size();
-        if(n1==0) return n2;
-        else if(n2==0) return n1;
         vector<vector<int>> dp(n1,vector<int>(n2,-1));
         return recur(n1-1,n2-1,s1,s2,dp);
     }
