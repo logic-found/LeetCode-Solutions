5b1f593e2943b9d28cf95b24561fec31c12a2122
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-22 23:42:39 +0530

    Time: 29 ms (44.67%), Space: 8.5 MB (71.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    //1 based index
+    //space optimised
     int minDistance(string s1, string s2) {
         int n1=s1.size(),n2=s2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1));
-        
-        for(int i=0;i<=n1;++i){   // base case-> j<0 =i   
-            dp[i][0]=i;
-        }
+        vector<int> dp(n2+1);
+       
         for(int j=0;j<=n2;++j){   // i<0 =j
-            dp[0][j]=j;
+            dp[j]=j;
         }
+        
         for(int i=1;i<=n1;++i){
+            vector<int> temp(n2+1);
+            temp[0]=i;
             for(int j=1;j<=n2;++j){
-                if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
+                if(s1[i-1]==s2[j-1]) temp[j]=dp[j-1];
                 else{
-                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+                   temp[j]=1+min(temp[j-1],min(dp[j-1],dp[j]));//dp[i][j-1], dp[i-1][j-1], dp[i-1][j]
                 }
             }
+            dp=temp;
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
