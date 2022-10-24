ac2f75f1b96fb00c5934279785cc343e4a044e6e
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-24 15:43:46 +0530

    Time: 102 ms (83.51%), Space: 20.8 MB (66.70%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    //space optimised
     int maximalSquare(vector<vector<char>>& mat) {
         int m=mat.size(),n=mat[0].size(),area=0;
-        vector<vector<int>> dp(m,vector<int>(n));
+        vector<int> dp(n);
     
         for(int i=0;i<m;++i){
+            vector<int> temp(n);
             for(int j=0;j<n;++j){
                 if(mat[i][j]=='1'){               // fill 1 area square
-                    dp[i][j]=1;
-                    area=max(area,dp[i][j]*dp[i][j]);
+                    temp[j]=1;
+                    area=max(area,1);
                 }
                 if(i>=1 && j>=1){
                     if(mat[i][j]=='1' && mat[i-1][j-1]=='1' && mat[i-1][j]=='1' && 
                                                                         mat[i][j-1]=='1'){
-                        dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
-                        area=max(area,dp[i][j]*dp[i][j]);
+                        temp[j]=1+min(dp[j-1],min(dp[j],temp[j-1]));
+                        area=max(area,temp[j]*temp[j]);
                     }
                 }
                 
             }
+            dp=temp;
         }
         
         return area;
