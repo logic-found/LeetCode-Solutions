e2c3a32e48a07518887488525af88b2d8ff3754a
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-10-24 07:42:11 +0530

    Time: 27 ms (5.20%), Space: 7.7 MB (5.15%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int recur(int i,int& n,int target,vector<int>& nums,vector<vector<int>>& dp){
+        if(target==0) return 1;
+        if(i>=n) return 0;
+        if(dp[i][target]!=-1) return dp[i][target];
+        
+        int temp=0;
+        for(int k=0;k<n;++k){
+            if(target-nums[k]>=0){
+                temp+=recur(k,n,target-nums[k],nums,dp);
+            }
+            
+        }
+        return dp[i][target]=temp;
+    }    
+    int combinationSum4(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
+        sort(nums.begin(),nums.end());
+        return recur(0,n,target,nums,dp);
+    }
+};
