cc52a3679fa59d892e1f4bf17a0d5c314aa2b9e6
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-11-01 08:08:19 +0530

    Time: 107 ms (74.13%), Space: 18.9 MB (68.94%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -5,7 +5,7 @@ public:
         if(m==1 && n==1) return 0;
         
         queue<vector<int>> q;
-        q.push({0,0,obstacle,0});  // row=0,col=0,obs=0,dis=0
+        q.push({0,0,0,0});  // row=0,col=0,obs=0,dis=0
         vector<vector<int>> dp(m,vector<int>(n,-1)); // store no of obstacles 
         vector<pair<int,int>> v={{1,0},{0,1},{0,-1},{-1,0}};
         
@@ -15,11 +15,11 @@ public:
             int row=k[0],col=k[1],obs=k[2],dis=k[3];
             if(row==m-1 && col==n-1) return dis;
             
-            if(mat[row][col]==1) --obs;
+            if(mat[row][col]==1) ++obs;
             
             for(auto [x,y]:v){
                 int i=row+x,j=col+y;
-                if(i>=0 && i<m && j>=0 && j<n && obs>dp[i][j]){
+                if(i>=0 && i<m && j>=0 && j<n && obs<=obstacle && (dp[i][j]==-1 || obs<dp[i][j])){
                     dp[i][j]=obs;
                     q.push({i,j,obs,dis+1});
                 }
