ae4d0392516767e93a766f296d45417c1d4d409d
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-11-02 22:47:04 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -5,56 +5,39 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
   public:
-    bool recur(int idx1,int idx2,string& s1,string& s2,vector<vector<int>>& dp){
-    	if(idx1==0 && idx2==0) return true;
-    	else if(idx2==0) return false;
-    	else if(s2[idx2-1]!='?' && s2[idx2-1]!='*' && (idx1>0 && s1[idx1-1]!=s2[idx2-1]))
-    	    return false;
-    	else if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-    	
-    	if(idx1>0 && s1[idx1-1]==s2[idx2-1]){
-    		return dp[idx1][idx2]=recur(idx1-1,idx2-1,s1,s2,dp);
-    	}
-    	else if(idx1>0 && s2[idx2-1]=='?'){
-    		return dp[idx1][idx2]=recur(idx1-1,idx2-1,s1,s2,dp);
-    	}
-    	else if(s2[idx2-1]=='*'){
-    		for(int i=0;idx1-i>=0;++i){
-    			bool a=recur(idx1-i,idx2-1,s1,s2,dp);
-    			if(a) return dp[idx1][idx2]=true;
-    		}
-    	}
-    	return dp[idx1][idx2]=false;
-    }
-    int wildCard(string pattern,string str)
+/*You are required to complete this method*/
+    int wildCard(string s2,string s1)
     {
-        int n1=str.size(),n2=pattern.size();
-        int size=(n1>n2)? n1:n2;
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-        // for(int r=0;r<=size;++r){
-        //     dp[r][n2]=0;
-        // }
-        // dp[n1][n2]=1;
-        // for(int idx1=n1-1;idx1>=0;--idx1){
-        //     for(int idx2=n2-1;idx2>=0;--idx2){
-                
-        //         if(s1[idx1]==s2[idx2] || s2[idx2]=='?'){
-        //             dp[idx1][idx2]=dp[idx1+1][idx2+1];
-        //         }
-        //         else{
-        //             for(int i=0;i+idx1<=n1;++i){
-        //                 bool a=dp[idx1+i][idx2+1];
-        //                 if(a){
-        //                     dp[idx1][idx2]=true;
-        //                     break;
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        //  ?*?*?*?*???****?????***z*?*       askfkaaasgasz
+        int n1=s1.size(),n2=s2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1));
+         
+        for(int i=1;i<=n2 && s2[i-1]=='*';++i){
+            dp[0][i]=1;
+        }
+        dp[0][0]=1;
+        for(int idx1=1;idx1<=n1;++idx1){
+            for(int idx2=1;idx2<=n2;++idx2){
+                if(s1[idx1-1]==s2[idx2-1]){
+                    dp[idx1][idx2]=dp[idx1-1][idx2-1];
+                }
+                else if(s2[idx2-1]=='?'){
+                    dp[idx1][idx2]=dp[idx1-1][idx2-1];
+                }
+                else if(s2[idx2-1]=='*'){
+                    for(int i=0;idx1-i>=0;++i){
+                        bool a=dp[idx1-i][idx2-1];
+                        if(a){
+                            dp[idx1][idx2]=1;
+                            break;
+                        }
+                    }
+                }
+                else dp[idx1][idx2]=0;
+            }
+        }
+        
+        return dp[n1][n2];
 
-        return recur(n1,n2,str,pattern,dp);
     }
 };
 
