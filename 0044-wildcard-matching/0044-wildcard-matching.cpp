bfd1401896e59ab8a093a9acd8194867832bdf91
Rashika Sahu <93260606+rashikasahu112@users.noreply.github.com>
2022-11-03 06:57:36 +0530

    Time: 57 ms (81.54%), Space: 26.1 MB (47.56%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     // 1 based indexing
     // tabulation 
-    
+    // 1-d
     bool isMatch(string s1, string s2) {
         int n1=s1.size(),n2=s2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1));
-        dp[0][0]=1;
+        vector<int> dp(n2+1);
+        dp[0]=1;
         for(int c=1;c<=n2 && s2[c-1]=='*';++c){
-            dp[0][c]=1;
+            dp[c]=1;
         }
         for(int i=1;i<=n1;++i){
+            vector<int> temp(n2+1);
             for(int j=1;j<=n2;++j){
                 if(s1[i-1]==s2[j-1] || s2[j-1]=='?'){
-                    dp[i][j]=dp[i-1][j-1];
+                    temp[j]=dp[j-1];
                 }
                 else if(s2[j-1]=='*'){
-                    dp[i][j]=dp[i][j-1] || dp[i-1][j];
+                    temp[j]=temp[j-1] || dp[j];
                 }
-                else dp[i][j]=0;
+                else temp[j]=0;
             }
+            dp=temp;
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
